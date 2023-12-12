56955a21243075dc2d0bdcf41ece3dfd53fe78fb
Long Tran <lk.longtn@gmail.com>
2023-12-12 22:55:44 +0700

    Update import page

--- src/app/modules/user/page/import/import.component.ts
+++ src/app/modules/user/page/import/import.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-import',
   templateUrl: './import.component.html',
-  styleUrls: ['./import.component.scss']
+  styleUrls: ['./import.component.scss'],
 })
 export class ImportComponent {
+  selectedFile: File | null = null;
+  @ViewChild('fileInput') fileInput!: ElementRef<any>;
 
+  constructor(private renderer: Renderer2) {}
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0] as File;
+  }
+
+  onSubmit() {
+    if (this.selectedFile) {
+      // Upload logic here
+      console.log('File uploaded:', this.selectedFile);
+    }
+  }
+
+  openFileInput() {
+    this.renderer.selectRootElement(this.fileInput.nativeElement).click();
+  }
 }
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -3,17 +3,11 @@ import { NgModule } from '@angular/core';
 import { UserRoutingModule } from './user-routing.module';
 import { SharedModule } from '@shared/shared.module';
 import { HomeComponent } from './page/home/home.component';
-import { ExampleFormComponent } from './page/example-form/example-form.component';
 import { ImportComponent } from './page/import/import.component';
 import { ReportComponent } from './page/report/report.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    ExampleFormComponent,
-    ImportComponent,
-    ReportComponent,
-  ],
+  declarations: [HomeComponent, ImportComponent, ReportComponent],
   imports: [UserRoutingModule, SharedModule],
 })
 export class UserModule {}
